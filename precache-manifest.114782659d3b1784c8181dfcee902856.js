9ec93d9566a20596d7d5329d31b1446d0788b0fd
Keith Ratner <keith.ratner@gmail.com>
2020-01-14 13:01:29 -0800

    Updates

---
+++ precache-manifest.114782659d3b1784c8181dfcee902856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb8005744c1e57a76138e357768b3cb",
+    "url": "/react-appointment-scheduler/index.html"
+  },
+  {
+    "revision": "86b89258d4f740092a57",
+    "url": "/react-appointment-scheduler/static/css/main.0a2a8514.chunk.css"
+  },
+  {
+    "revision": "de30f243c2c6cbed15fd",
+    "url": "/react-appointment-scheduler/static/js/2.5613b4c0.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/react-appointment-scheduler/static/js/2.5613b4c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86b89258d4f740092a57",
+    "url": "/react-appointment-scheduler/static/js/main.717dcacf.chunk.js"
+  },
+  {
+    "revision": "73d5ff70484be4c4c114",
+    "url": "/react-appointment-scheduler/static/js/runtime-main.ebc09f7d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6196c453d5def5f6b68bb78f89d869e.js"
+  "/react-appointment-scheduler/precache-manifest.114782659d3b1784c8181dfcee902856.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-appointment-scheduler/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
